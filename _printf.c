9a6e5fae6ad2ee6abd9cf9bf49a49cc6b6f53fbb
ghofranaaa <smailikhaled841@gmail.com>
2024-03-27 00:25:40 +0100

    correcting prototype name

--- _printf.c
+++ _printf.c
@@ -4,38 +4,48 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;                    // Declare a variable to hold the variable arguments
-    int count = 0;                   // Initialize a counter for the number of characters printed
-    const char *ptr = format;        // Initialize a pointer to the format string
+	va_list args;			  // Declare a variable to hold the variable arguments
+	int count = 0;			  // Initialize a counter for the number of characters printed
+	const char *ptr = format; // Initialize a pointer to the format string
 
-    va_start(args, format);          // Start processing the variable arguments
+	va_start(args, format); // Start processing the variable arguments
 
-    while (*ptr) {                   // Loop 
-        if (*ptr != '%') {           // If the character is not a '%'
-            write(1, ptr, 1);        // Write the character to the standard output
-            count++;                 // counter
-        } else {                     // If the character is a '%'
-            ptr++;                   // Move to the next character
-            if (*ptr == 'c') {       // If it is 'c'
-                char c = va_arg(args, int);  // get a character argument
-                write(1, &c, 1);      // Write the character to the standard output
-                count++;
-            } else if (*ptr == 's') {  // If the conversion specifier is 's'
-                char *s = va_arg(args, char *);  // get a string argument
-                while (*s) {         // Loop
-                    write(1, s, 1);  // Write the character to the standard output
-                    s++;             // Move to the next character in the string
-                    count++;
-                }
-            } else if (*ptr == '%') {  // If the conversion specifier is '%%'
-                write(1, "%", 1);     // Write a '%' character to the standard output
-                count++;
-            }
-        }
-        ptr++;                       // Move to the next character in the format string
-    }
+	while (*ptr)
+	{ // Loop
+		if (*ptr != '%')
+		{					  // If the character is not a '%'
+			write(1, ptr, 1); // Write the character to the standard output
+			count++;		  // counter
+		}
+		else
+		{		   // If the character is a '%'
+			ptr++; // Move to the next character
+			if (*ptr == 'c')
+			{								// If it is 'c'
+				char c = va_arg(args, int); // get a character argument
+				write(1, &c, 1);			// Write the character to the standard output
+				count++;
+			}
+			else if (*ptr == 's')
+			{									// If the conversion specifier is 's'
+				char *s = va_arg(args, char *); // get a string argument
+				while (*s)
+				{					// Loop
+					write(1, s, 1); // Write the character to the standard output
+					s++;			// Move to the next character in the string
+					count++;
+				}
+			}
+			else if (*ptr == '%')
+			{					  // If the conversion specifier is '%%'
+				write(1, "%", 1); // Write a '%' character to the standard output
+				count++;
+			}
+		}
+		ptr++; // Move to the next character in the format string
+	}
 
-    va_end(args);                   // End
+	va_end(args); // End
 
-    return count;                   // Return the total number of characters printed
+	return count; // Return the total number of characters printed
 }
--- main.h
+++ main.h
@@ -12,7 +12,7 @@ char print_string(va_list arg);
 char print_character(va_list arg);
 char print_percent(va_list arg);
 int print_integer(va_list arg);
-int (*get_func(char format)(va_list arg));
+int (*get_func(char format))(va_list arg);
 
 /**
  * struct fmt - struct declaration.
