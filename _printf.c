a28e0f70a9a97de10cc7c6cd29bf1469ce8732f3
oussematurki0 <oussematurki0@gmail.com>
2024-03-28 22:33:10 +0100

    function implementation

--- _printf.c
+++ _printf.c
@@ -30,7 +30,7 @@ int _printf(const char *format, ...)
 			if (*ptr == '\0')   /* If the next char is null terminator, return -1 */
 				return (-1);
 
-			count += handle_format_specifier(*ptr, args);
+			count += Jump_to_secand_specifier(*ptr, args);
 		}
 		ptr++;                 /* Move to the next character in the format str */
 	}
@@ -40,13 +40,13 @@ int _printf(const char *format, ...)
 }
 
 /**
- * handle_format_specifier - Handles the printing for a format specifier
+ * Jump_to_secand_specifier - Handles the printing for a format specifier
  * @specifier: The format specifier character
  * @args: The va_list containing the arguments
  *
  * Return: The number of characters printed
  */
-int handle_format_specifier(char specifier, va_list args)
+int Jump_to_secand_specifier(char specifier, va_list args)
 {
     int count = 0;
 
@@ -69,5 +69,5 @@ int handle_format_specifier(char specifier, va_list args)
         break;
     }
 
-    return (count); // <-- Added parentheses around count
+    return (count); /* Added parentheses around count */
 }
