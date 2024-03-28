c0dd588d9f5eab451eeee2f66096dae72270bd5f
oussematurki0 <oussematurki0@gmail.com>
2024-03-28 22:21:36 +0100

    function implementation

--- _printf.c
+++ _printf.c
@@ -12,22 +12,22 @@ int _printf(const char *format, ...)
 {
 	va_list args;               /* Declare a var to hold the var arguments */
 	int count = 0;              /* Init a counter */
-	const char *ptr = format;   /* Initialize a pointer to the format string */
-	va_start(args, format);     /* Start processing the variable arguments */
+	const char *ptr = format;   /* Init a pointer to the format str */
+	va_start(args, format);     /* Start process the var arguments */
 	while (*ptr)
 	{
 		if (*ptr != '%')
-		{                       /* If the character is not a '%' */
-			_putchar(*ptr);     /* Writeee the character to the standard output */
-			count++;            /* Increment the character count */
+		{                       /* If the character is not '%' */
+			_putchar(*ptr);     /* Write the char to the standard output */
+			count++;
 		}
 		else
-		{                       /* If the character is a '%' */
-			ptr++;              /* Move to the next character */
+		{                       /* If the character is '%' */
+			ptr++;              /* Move to the next char */
 			if (*ptr == '\0')   /* If the next char is null terminator, return -1 */
 				return (-1);
 
-			switch (*ptr)       /* Switch stat to handle diff format specifiers */
+			switch (*ptr)       /* Switch stat to handle diff format specif */
 			{
 			case 'c':           /* If the specifier is 'c' */
 				count += print_character(args);
@@ -41,12 +41,12 @@ int _printf(const char *format, ...)
 				break;
 			default:
 				_putchar('%');  /* Print '%' */
-				_putchar(*ptr); /* Print the next character */
+				_putchar(*ptr); /* Print the next char */
 				count += 2;     /* Increment the count by 2 */
 				break;
 			}
 		}
-		ptr++;                 /* Move to the next character in the format string */
+		ptr++;                 /* Move to the next character in the format str */
 	}
 	va_end(args);             /* End */
 	return (count);           /* Return the total number of characters printed */
