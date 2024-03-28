7c6e1897c77a68bddc3a8c16137f7c475d24627e
oussematurki0 <oussematurki0@gmail.com>
2024-03-28 22:37:06 +0100

    function implementation

--- _printf.c
+++ _printf.c
@@ -10,33 +10,33 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;               /* Declare a var to hold the var arguments */
-	int count = 0;              /* Init a counter */
+    va_list args;               /* Declare a var to hold the var arguments */
+    int count = 0;              /* Init a counter */
 
-	const char *ptr = format;   /* Init a pointer to the format str */
+    const char *ptr = format;   /* Init a pointer to the format str */
 
-	va_start(args, format);     /* Start process the var arguments */
+    va_start(args, format);     /* Start process the var arguments */
 
-	while (*ptr)
-	{
-		if (*ptr != '%')
-		{                       /* If the character is not '%' */
-			_putchar(*ptr);     /* Write the char to the standard output */
-			count++;
-		}
-		else
-		{                       /* If the character is '%' */
-			ptr++;              /* Move to the next char */
-			if (*ptr == '\0')   /* If the next char is null terminator, return -1 */
-				return (-1);
+    while (*ptr)
+    {
+        if (*ptr != '%')
+        {                       /* If the character is not '%' */
+            _putchar(*ptr);     /* Write the char to the standard output */
+            count++;
+        }
+        else
+        {                       /* If the character is '%' */
+            ptr++;              /* Move to the next char */
+            if (*ptr == '\0')   /* If the next char is null terminator, return -1 */
+                return (-1);
 
-			count += Jump_to_secand_specifier(*ptr, args);
-		}
-		ptr++;                 /* Move to the next character in the format str */
-	}
+            count += Jump_to_secand_specifier(*ptr, args);
+        }
+        ptr++;                 /* Move to the next character in the format str */
+    }
 
-	va_end(args);             /* End */
-	return (count);           /* Return the total number of characters printed */
+    va_end(args);             /* End */
+    return (count);           /* Return the total number of characters printed */
 }
 
 /**
@@ -48,26 +48,26 @@ int _printf(const char *format, ...)
  */
 int Jump_to_secand_specifier(char specifier, va_list args)
 {
-int count = 0;
+    int count = 0;
 
-switch (specifier)
-{
-case 'c':           /* If the specifier is 'c' */
-count += print_character(args);
-break;
-case 's':           /* If the specifier is 's' */
-count += print_string(args);
-break;
-case '%':           /* If the specifier is '%' */
-_putchar('%');
-count++;
-break;
-default:
-_putchar('%');  /* Print '%' */
-_putchar(specifier); /* Print the specifier character */
-count += 2;     /* Increment the count by 2 */
-break;
-}
+    switch (specifier)
+    {
+    case 'c':           /* If the specifier is 'c' */
+        count += print_character(args);
+        break;
+    case 's':           /* If the specifier is 's' */
+        count += print_string(args);
+        break;
+    case '%':           /* If the specifier is '%' */
+        _putchar('%');
+        count++;
+        break;
+    default:
+        _putchar('%');  /* Print '%' */
+        _putchar(specifier); /* Print the specifier character */
+        count += 2;     /* Increment the count by 2 */
+        break;
+    }
 
-return (count); /* Added parentheses around count */
+    return (count); /* Added parentheses around count */
 }
