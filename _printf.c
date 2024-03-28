7505c9fa85b869ff133a025396ef2a44a5e99116
oussematurki0 <oussematurki0@gmail.com>
2024-03-28 22:28:26 +0100

    function implementation

--- _printf.c
+++ _printf.c
@@ -30,28 +30,44 @@ int _printf(const char *format, ...)
 			if (*ptr == '\0')   /* If the next char is null terminator, return -1 */
 				return (-1);
 
-			switch (*ptr)       /* Switch stat to handle diff format specif */
-			{
-			case 'c':           /* If the specifier is 'c' */
-				count += print_character(args);
-				break;
-			case 's':           /* If the specifier is 's' */
-				count += print_string(args);
-				break;
-			case '%':           /* If the specifier is '%' */
-				_putchar('%');
-				count++;
-				break;
-			default:
-				_putchar('%');  /* Print '%' */
-				_putchar(*ptr); /* Print the next char */
-				count += 2;     /* Increment the count by 2 */
-				break;
-			}
+			count += handle_format_specifier(*ptr, args);
 		}
 		ptr++;                 /* Move to the next character in the format str */
 	}
 
 	va_end(args);             /* End */
 	return (count);           /* Return the total number of characters printed */
+}
+
+/**
+ * handle_format_specifier - Handles the printing for a format specifier
+ * @specifier: The format specifier character
+ * @args: The va_list containing the arguments
+ *
+ * Return: The number of characters printed
+ */
+int handle_format_specifier(char specifier, va_list args)
+{
+	int count = 0;
+
+	switch (specifier)
+	{
+	case 'c':           /* If the specifier is 'c' */
+		count += print_character(args);
+		break;
+	case 's':           /* If the specifier is 's' */
+		count += print_string(args);
+		break;
+	case '%':           /* If the specifier is '%' */
+		_putchar('%');
+		count++;
+		break;
+	default:
+		_putchar('%');  /* Print '%' */
+		_putchar(specifier); /* Print the specifier character */
+		count += 2;     /* Increment the count by 2 */
+		break;
+	}
+
+	return count;
 }
