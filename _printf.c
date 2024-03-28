73f8592d1b1dba851e597f631fc60b4248e461e3
oussematurki0 <oussematurki0@gmail.com>
2024-03-28 22:31:16 +0100

    function implementation

--- _printf.c
+++ _printf.c
@@ -30,7 +30,7 @@ int _printf(const char *format, ...)
 			if (*ptr == '\0')   /* If the next char is null terminator, return -1 */
 				return (-1);
 
-			count += handle_format_specifier(*ptr, args);
+			count += Jump_to_secand_specifier(*ptr, args);
 		}
 		ptr++;                 /* Move to the next character in the format str */
 	}
@@ -40,34 +40,34 @@ int _printf(const char *format, ...)
 }
 
 /**
- * handle_format_specifier - Handles the printing for a format specifier
+ * Jump_to_secand_specifier - Handles the printing for a format specifier
  * @specifier: The format specifier character
  * @args: The va_list containing the arguments
  *
  * Return: The number of characters printed
  */
-int handle_format_specifier(char specifier, va_list args)
+int Jump_to_secand_specifier(char specifier, va_list args)
 {
-	int count = 0;
+    int count = 0;
 
-	switch (specifier)
-	{
-	case 'c':           /* If the specifier is 'c' */
-		count += print_character(args);
-		break;
-	case 's':           /* If the specifier is 's' */
-		count += print_string(args);
-		break;
-	case '%':           /* If the specifier is '%' */
-		_putchar('%');
-		count++;
-		break;
-	default:
-		_putchar('%');  /* Print '%' */
-		_putchar(specifier); /* Print the specifier character */
-		count += 2;     /* Increment the count by 2 */
-		break;
-	}
+    switch (specifier)
+    {
+    case 'c':           /* If the specifier is 'c' */
+        count += print_character(args);
+        break;
+    case 's':           /* If the specifier is 's' */
+        count += print_string(args);
+        break;
+    case '%':           /* If the specifier is '%' */
+        _putchar('%');
+        count++;
+        break;
+    default:
+        _putchar('%');  /* Print '%' */
+        _putchar(specifier); /* Print the specifier character */
+        count += 2;     /* Increment the count by 2 */
+        break;
+    }
 
-	return count;
+    return (count); // <-- Added parentheses around count
 }
