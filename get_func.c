672b5e0b5e6e5385e8bce3a64ff8bed3b2c57e62
oussematurki0 <oussematurki0@gmail.com>
2024-03-28 22:40:12 +0100

    fixed error

--- get_func.c
+++ get_func.c
@@ -15,25 +15,22 @@
 
 int (*get_func(char format))(va_list arg)
 {
-	f fu[] = {
-		{'c', print_character}, /* Character specifier */
-		{'s', print_string},    /* String specifier */
-		{'%', print_percent},   /* Percent specifier */
-		{'\0', NULL},           /* Null terminator to indicate end of array */
+    f fu[] = {
+        {'c', print_character}, /* Character specifier */
+        {'s', print_string},    /* String specifier */
+        {'%', print_percent},   /* Percent specifier */
+        {'\0', NULL},           /* Null terminator to indicate end of array */
+    };
 
+    int i; /* Declare an integer variable i */
 
-	};
+    for (i = 0; fu[i].str != '\0'; i++) /* Loop through the array until null terminator is reached */
+    {
+        if (fu[i].str == format) /* Check if the current format specifier matches the input format */
+        {
+            return (fu[i].p); /* Return the corresponding function pointer */
+        }
+    }
 
-	int i; /* Declare an integer variable i */
-
-	for (i = 0; fu[i].str != '\0'; i++) /* Loop through the array until null terminator is reached */
-	{
-		if (fu[i].str == format) /* Check if the current format specifier matches the input format */
-		{
-			return (fu[i].p); /* Return the corresponding function pointer */
-		}
-	}
-
-	return (NULL); /* If no match is found, return NULL */
-
+    return (NULL); /* If no match is found, return NULL */
 }
