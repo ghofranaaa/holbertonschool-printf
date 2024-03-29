fe26e855ee5f4d1865943cc5825b0a46da1e6ec7
oussematurki0 <oussematurki0@gmail.com>
2024-03-29 10:02:45 +0100

    updated

--- _printf.c
+++ _printf.c
@@ -3,60 +3,60 @@
 
 /**
  * _printf - Prints formatted output to stdout
- * @format: The format string
+ * @format: The format str
  *
- * Return: The number of characters printed
+ * Return: The number of char printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    const char *ptr = format;
-    int (*func_p)(va_list) = NULL;
+    va_list args;                                    /* Var arguments list */
+    int count = 0;                                   /* Init counter for chars printed */
+    const char *ptr = format;                        /* Init pointer to format str */
+    int (*func_p)(va_list) = NULL;                   /* Init function pointer to NULL */
 
-    if (format == NULL)
+    if (format == NULL)                              /* Check if format str is NULL */
         return (-1);
 
-    va_start(args, format);
+    va_start(args, format);                          /* Start var arguments list */
 
-    while (*ptr)
+    while (*ptr)                                     /* Loop through format str */
     {
-        if (*ptr != '%')
+        if (*ptr != '%')                             /* If current char is not '%' */
         {
-            _putchar(*ptr);
-            count++;
+            _putchar(*ptr);                          /* Print the char */
+            count++;                                 /* Increment char count */
         }
-        else
+        else                                         /* If current char is '%' */
         {
-            ptr++;
-            if (*ptr == '\0')
-                return (-1);
+            ptr++;                                   /* Move to next char */
+            if (*ptr == '\0')                        /* If next char is NULL */
+                return (-1);                         /* Return -1 */
 
-            func_p = get_func(*ptr);
+            func_p = get_func(*ptr);                 /* Get function pointer for specifier */
 
-            if (func_p != NULL)
-                count += func_p(args);
-            else
+            if (func_p != NULL)                      /* If valid function pointer found */
+                count += func_p(args);               /* Call function and update count */
+            else                                     /* If no valid function pointer found */
             {
-                if (*ptr == 's')
-                    count += print_string(args);
-                else if (*ptr == 'c')
-                    count += print_character(args);
-                else if (*ptr == '%')
-                    count += print_percent(args);
-                else if (*ptr == 'd' || *ptr == 'i')
-                    count += print_integer(args);
-                else
+                if (*ptr == 's')                     /* If specifier is 's' */
+                    count += print_string(args);     /* Call print_string and update count */
+                else if (*ptr == 'c')                /* If specifier is 'c' */
+                    count += print_character(args);  /* Call print_character and update count */
+                else if (*ptr == '%')                /* If specifier is '%' */
+                    count += print_percent(args);    /* Call print_percent and update count */
+                else if (*ptr == 'd' || *ptr == 'i') /* If specifier is 'd' or 'i' */
+                    count += print_integer(args);    /* Call print_integer and update count */
+                else                                 /* If unknown specifier */
                 {
-                    _putchar('%');
-                    _putchar(*ptr);
-                    count += 2;
+                    _putchar('%');                   /* Print '%' */
+                    _putchar(*ptr);                  /* Print specifier char */
+                    count += 2;                      /* Increment count by 2 */
                 }
             }
         }
-        ptr++;
+        ptr++;                                       /* Move to next char in format str */
     }
 
-    va_end(args);
+    va_end(args);                                    /* End */
     return (count);
 }
