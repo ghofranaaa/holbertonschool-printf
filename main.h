0a2196fe2cc99d90c9a0b5c58a40c5240f184cfa
oussematurki0 <oussematurki0@gmail.com>
2024-03-29 01:58:49 +0100

    fixed error

--- main.h
+++ main.h
@@ -12,6 +12,7 @@ int print_character(va_list args);
 int print_percent(va_list args);
 int (*get_func(char format))(va_list);
 int _putchar(char c);
+int handle_format(const char *format, va_list args);
 
 /**
  * struct fmt - struct declaration.
